cc85beca63ccb2240c4bc74774448c9545f2c813
sternlucas <contas.lucas@gmail.com>
2025-06-22 17:27:30 -0300

    feat: add product validator and notification

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -3,13 +3,13 @@ import Product from "./product";
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
-      const product = new Product("", "Product 1", 100);
+      new Product("", "Product 1", 100);
     }).toThrowError("Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
-      const product = new Product("123", "", 100);
+      new Product("123", "", 100);
     }).toThrowError("Name is required");
   });
 
